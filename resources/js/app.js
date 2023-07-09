cbcd9e65e7a9cb714f9f5e5d641d019d8585c199
Artem Pokhiliuk <timtimoha@yandex.ru>
2023-07-10 01:26:28 +0600

    Attempt to fix issue with Alpine.js not working in prod

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,25 +1,8 @@
 import './bootstrap';
 import ujs from '@rails/ujs';
 import Alpine from 'alpinejs';
-import {createApp} from 'vue';
-import {createRouter, createWebHistory} from 'vue-router'
-import App from './App.vue';
-import ProdList from './components/Product-listing.vue';
 
 window.Alpine = Alpine;
 
 ujs.start();
 Alpine.start();
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {
-            path: '/products',
-            name: 'Products',
-            component: ProdList
-        },
-    ]
-})
-
-createApp(App).use(router).mount("#prodlist");
